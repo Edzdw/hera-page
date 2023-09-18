754dba63904983f3d6b4938ef3d2cad1b0f4d049
edzdw <minhtam09081999@gmail.com>
2023-09-18 21:00:03 +0700

    first update

---
+++ src/components/sections/About.js
@@ -0,0 +1,318 @@
+const About = () => {
+  return (
+    <section className="about main-section flex-column-mobile" id="about">
+      {/* INFO HOLDER STARTS */}
+      <div className="info flex-column-mobile">
+        {/* IMAGE STARTS */}
+        <div
+          className="img-container animated-layer image-animation my-photo-container fadeInUp wow"
+          data-wow-offset={200}
+          id="my-photo"
+        >
+          <div>
+            <div>
+              <img className="my-photo" src="assets/about.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        {/* IMAGE ENDS */}
+        {/* INFO STARTS */}
+        <div>
+          <h2>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Alex
+              </span>
+            </span>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Trần Tường Thụy
+              </span>
+            </span>
+          </h2>
+          <div className="infos">
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Kinh nghiệm :</span>
+                    <span>20 năm trong lĩnh vực đào tạo  </span>
+                    <p>& kèm cặp tài chính & bảo hiểm cá nhân</p>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Quốc tịch :</span>
+                    <span>VietNam</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Chức vụ:</span>
+                    <span>RD (Regional Director, FWD)</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Ngôn ngữ :</span>
+                    <span>Tiếng anh, Tiếng Việt</span>
+                  </span>
+                </span>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Địa chỉ :</span>
+                    <span>Can Tho, HCMC</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Số Điện Thoại :</span>
+                    <span>+84939635089</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Email :</span>
+                    <span> alexfinance101@outlook.com</span>
+                  </span>
+                </span>
+              </li>
+              {/* <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Accomplishments :</span>
+                    <span>Building new Market & new GA in Mekong Delta</span>
+                    <p>Building 10 Zone Head currently.</p>
+                    <span>Building 833 agency & 99 Agency leaders at the end of 31 July 2022.</span>
+                  </span>
+                </span>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+        {/* INFO ENDS */}
+      </div>
+      {/* INFO HOLDER ENDS */}
+      {/* SKILLS STARTS */}
+      <div className="skills flex-column-mobile">
+        {/* TITLE STARTS */}
+        <div className="custom-title">
+          {/* MAIN TITLE STARTS */}
+          <h3>
+            <span>
+              <span className="animated-layer fade-in-left-animation fadeInUp wow">
+                Tính năng
+              </span>
+            </span>
+          </h3>
+          {/* MAIN TITLE ENDS */}
+        </div>
+        {/* TITLE ENDS */}
+        <div className="skills-content">
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-wordpress-plain" />
+              </span>
+              <h4>Tự động giao dịch</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-jquery-plain" />
+              </span>
+              <h4>24/5</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-angularjs-plain" />
+              </span>
+              <h4>Phòng chống rủi ro</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-drupal-plain" />
+              </span>
+              <h4>Mốc bảo vệ</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-react-plain" />
+              </span>
+              <h4>Phân tích real-time</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-docker-plain" />
+              </span>
+              <h4>Phân tích chiến lược</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-nodejs-plain" />
+              </span>
+              <h4> Vào/thoát lệnh </h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-sass-plain" />
+              </span>
+              <h4>Đa dạng cặp tiền</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+        </div>
+      </div>
+      {/* SKILLS ENDS */}
+      {/* RESUME STARTS */}
+      <div className="resume flex-column-mobile">
+        {/* TITLE STARTS */}
+        <div className="custom-title fadeInUp wow">
+          {/* MAIN TITLE STARTS */}
+          <h3>
+            <span>
+              <span className="animated-layer fade-in-left-animation">
+                Thành tựu
+              </span>
+            </span>
+          </h3>
+          {/* MAIN TITLE ENDS */}
+        </div>
+        {/* TITLE ENDS */}
+        {/* TIMELINE STARTS */}
+        <div className="timeline">
+          <ol className="animated-layer fade-in-animation">
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Infinity 1</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>10 2022</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span> Sóng Hồi</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Infinity 4</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>4 2023</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Giảm vị thế xấu</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Infinity 2</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>12 2022</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Giao dịch 2 chiều</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="education">
+                  <h4>Infinity EA</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2023+</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>Đang phát triển</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="education">
+                  <h4>Infinity 3</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>3 2023</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>Tự Xóa Lệnh</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            <li />
+          </ol>
+        </div>
+        {/* TIMELINE ENDS */}
+      </div>
+      {/* RESUME ENDS */}
+      <img
+        alt=""
+        className="separator hide-mobile"
+        src="assets/separator.png"
+      />
+    </section>
+  );
+};
+export default About;
