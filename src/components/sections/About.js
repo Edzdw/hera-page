cf3dd3773c5bd7bf9709479d2760d7119fa20faf
edzdw <minhtam09081999@gmail.com>
2023-09-19 13:52:37 +0700

    update layout

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -37,7 +37,11 @@ const About = () => {
                   <span className="content-containers  animated-layer fade-in-up-animation fadeInUp wow">
                     <div class = "infos_contents">
                     Sự vận hành vạn vật luôn tuân theo các nguyên lý của tự nhiên… Khi tĩnh lặng thì tự khắc sẽ cảm nhận được về chân lý, mà chân lý luôn là những điều rất đơn giản và cũng rất giản dị… 
-                    Quy Luật Tài chính cũng như thế, Nó chỉ là phương tiện nó có thể đưa bạn vào vùng tối tăm hoặc cũng có thể đưa bạn đến vùng ánh sáng êm dịu, 
+                    Quy Luật Tài chính cũng như thế
+                    </div>
+
+                    <div class = "infos_contents">
+                    Nó chỉ là phương tiện nó có thể đưa bạn vào vùng tối tăm hoặc cũng có thể đưa bạn đến vùng ánh sáng êm dịu, 
                     quan trọng là góc nhìn của bạn đối với Nó như thế nào mà thôi… Nếu Bạn được cam kết đến vùng ánh sáng êm dịu thì Bạn có cam kết lại … để rồi từng ngày sự hòa hợp luôn được tốt hơn !!!
                     </div>
 
