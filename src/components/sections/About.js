653b2a203f747efb9b2342ec807bc23d9078d632
edzdw <minhtam09081999@gmail.com>
2023-09-19 12:34:52 +0700

    update layout

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -35,7 +35,11 @@ const About = () => {
               <li>
                 <span>
                   <span className="animated-layer fade-in-up-animation fadeInUp wow">
-                    <span>Sự vận hành vạn vật luôn tuân theo các nguyên lý của tự nhiên… Khi tĩnh lặng thì tự khắc sẽ cảm nhận được về chân lý, mà chân lý luôn là những điều rất đơn giản và cũng rất giản dị… Quy Luật Tài chính cũng như thế, Nó chỉ là phương tiện nó có thể đưa bạn vào vùng tối tăm hoặc cũng có thể đưa bạn đến vùng ánh sáng êm dịu, quan trọng là góc nhìn của bạn đối với Nó như thế nào mà thôi… Nếu Bạn được cam kết đến vùng ánh sáng êm dịu thì Bạn có cam kết lại… để rồi từng ngày sự hòa hợp luôn được tốt hơn !!! </span>
+                    <p>Sự vận hành vạn vật luôn tuân theo các nguyên lý của tự nhiên… </p>
+                    <p>Khi tĩnh lặng thì tự khắc sẽ cảm nhận được về chân lý, mà chân lý luôn là những điều rất đơn giản và cũng rất giản dị…</p>
+                    <p>Quy Luật Tài chính cũng như thế, Nó chỉ là phương tiện nó có thể đưa bạn vào vùng tối tăm hoặc cũng có thể đưa bạn đến vùng ánh sáng êm dịu</p>
+                    <p>, quan trọng là góc nhìn của bạn đối với Nó như thế nào mà thôi…!!</p>
+                    <p>Nếu Bạn được cam kết đến vùng ánh sáng êm dịu thì Bạn có cam kết lại… để rồi từng ngày sự hòa hợp luôn được tốt hơn !</p>
                   </span>
                 </span>
               </li>
